dcd54252d39075e10fc2f52119a730d8ba6b1113
30isnottoolate <akos.varga.92@gmail.com>
2022-09-16 18:50:03 +0200

    Add state values

--- src/Teleprompter.js
+++ src/Teleprompter.js
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import './Teleprompter.css';
 
 function Teleprompter() {
+  const [isActive, setIsActive] = useState(false);
+  const [fontSize, setFontSize] = useState(100);
+  const [lineHeight, setLineHeight] = useState(1.2);
+  const [textSpeed, setTextSpeed] = useState(100);
+  
   return (
     <div id="app">
       <p>
