e9ebf3c30ac0cc341c98386d36f29d0838491a5a
30isnottoolate <akos.varga.92@gmail.com>
2022-09-18 19:59:35 +0200

    Add elements and read props from parent component

--- src/Controller.js
+++ src/Controller.js
@@ -1,9 +1,11 @@
 import './Teleprompter.css';
 
-function Controller() {
+function Controller(props) {
   return (
     <div id="controller">
-
+      <input type="range" id="font-size" min="80" max="150" step="1" value={props.font} />
+      <input type="range" id="line-height" min="1" max="1.5" step="0.01" value={props.height} />
+      <input type="range" id="text-speed" min="20" max="200" step="1" value={props.speed} />
     </div>
   );
 }
