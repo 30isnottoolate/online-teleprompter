50518780619ccd02281809c4bd1c95c7af38e945
30isnottoolate <akos.varga.92@gmail.com>
2022-09-19 22:08:27 +0200

    Add state liftup functions to component

--- src/Controller.js
+++ src/Controller.js
@@ -1,11 +1,23 @@
 import './Teleprompter.css';
 
-function Controller(props) {
+const Controller = (props) => {
+  const handleFontSize = (e) => {
+    props.setFontSize(e.target.value);
+  }
+
+  const handleLineHeight = (e) => {
+    props.setLineHeight(e.target.value);
+  }
+
+  const handleTextSpeed = (e) => {
+    props.setTextSpeed(e.target.value);
+  }
+
   return (
     <div id="controller">
-      <input type="range" id="font-size" min="80" max="150" step="1" value={props.font} />
-      <input type="range" id="line-height" min="1" max="1.5" step="0.01" value={props.height} />
-      <input type="range" id="text-speed" min="20" max="200" step="1" value={props.speed} />
+      <input type="range" id="font-size" min="80" max="150" step="1" value={props.font} onChange={handleFontSize} />
+      <input type="range" id="line-height" min="1" max="1.5" step="0.01" value={props.height} onChange={handleLineHeight} />
+      <input type="range" id="text-speed" min="20" max="200" step="1" value={props.speed} onChange={handleTextSpeed} />
     </div>
   );
 }
