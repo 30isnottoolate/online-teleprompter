14dc34fbaa55fd52d0fc1c2947d5137b5e203173
30isnottoolate <akos.varga.92@gmail.com>
2022-09-19 22:39:42 +0200

    Add changeStatus to main component

--- src/Teleprompter.js
+++ src/Teleprompter.js
@@ -3,17 +3,22 @@ import './Teleprompter.css';
 import Controller from './Controller.js';
 import Slider from './Slider';
 
-function Teleprompter() {
+const Teleprompter = () => {
   const [isActive, setIsActive] = useState(false);
   const [text, setText] = useState("Default text.")
   const [fontSize, setFontSize] = useState(100);
   const [lineHeight, setLineHeight] = useState(1.2);
   const [textSpeed, setTextSpeed] = useState(100);
 
+  const changeStatus = () => {
+    isActive ? setIsActive(false) : setIsActive(true)
+  }
+
   return (
     <div id="teleprompter">
       <Controller font={fontSize} setFontSize={setFontSize} height={lineHeight} setLineHeight={setLineHeight} speed={textSpeed} setTextSpeed={setTextSpeed} />
       <Slider text={text} setText={setText} />
+      <button onClick={changeStatus} >{isActive ? "Stop" : "Start"}</button>
     </div>
   );
 }
