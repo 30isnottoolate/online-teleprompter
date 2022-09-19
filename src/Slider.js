7e000246a30468922ca46804f03d10c5952c9050
30isnottoolate <akos.varga.92@gmail.com>
2022-09-19 22:30:15 +0200

    Create and implement handleTextChange

--- src/Slider.js
+++ src/Slider.js
@@ -1,13 +1,13 @@
 import './Teleprompter.css';
 
-const Slider = () => {
+const Slider = (props) => {
   const handleTextChange = (e) => {
-
+    props.setText(e.target.value);
   }
 
   return (
     <div id="text-slider">
-      <textarea id="text-container" value={text} onChange={handleTextChange} />
+      <textarea id="text-container" value={props.text} onChange={handleTextChange} />
     </div>
   );
 }
