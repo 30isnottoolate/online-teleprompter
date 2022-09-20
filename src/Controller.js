31806dc387ce749b1f1d66c67680824030d892e1
30isnottoolate <akos.varga.92@gmail.com>
2022-09-20 15:34:13 +0200

    Fix prop names

--- src/Controller.js
+++ src/Controller.js
@@ -1,25 +1,25 @@
 import './Teleprompter.css';
 
 const Controller = (props) => {
-  const handleFontSize = (e) => {
-    props.setFontSize(e.target.value);
-  }
+    const handleFontSize = (e) => {
+        props.setFontSize(e.target.value);
+    }
 
-  const handleLineHeight = (e) => {
-    props.setLineHeight(e.target.value);
-  }
+    const handleLineHeight = (e) => {
+        props.setLineHeight(e.target.value);
+    }
 
-  const handleTextSpeed = (e) => {
-    props.setTextSpeed(e.target.value);
-  }
+    const handleTextSpeed = (e) => {
+        props.setTextSpeed(e.target.value);
+    }
 
-  return (
-    <div id="controller">
-      <input type="range" id="font-size" min="80" max="150" step="1" value={props.font} onChange={handleFontSize} />
-      <input type="range" id="line-height" min="1" max="1.5" step="0.01" value={props.height} onChange={handleLineHeight} />
-      <input type="range" id="text-speed" min="20" max="200" step="1" value={props.speed} onChange={handleTextSpeed} />
-    </div>
-  );
+    return (
+        <div id="controller">
+            <input type="range" id="font-size" min="80" max="150" step="1" value={props.fontSize} onChange={handleFontSize} />
+            <input type="range" id="line-height" min="1" max="1.5" step="0.01" value={props.lineHeight} onChange={handleLineHeight} />
+            <input type="range" id="text-speed" min="20" max="200" step="1" value={props.textSpeed} onChange={handleTextSpeed} />
+        </div>
+    );
 }
 
 export default Controller;
