8c4448dcfc6a587c4faf0d4d1fc161bc4c78a231
30isnottoolate <akos.varga.92@gmail.com>
2022-09-20 15:56:37 +0200

    Replace textarea with an editable div

--- src/Slider.js
+++ src/Slider.js
@@ -1,15 +1,22 @@
 import './Teleprompter.css';
 
 const Slider = (props) => {
-  const handleTextChange = (e) => {
-    props.setText(e.target.value);
-  }
+    const handleTextChange = (e) => {
+        props.setText(e.target.value);
+    }
 
-  return (
-    <div id="text-slider" style={{position: "absolute", top: props.position}}>
-      <textarea id="text-container" style={{fontSize: props.fontSize + "px", lineHeight: props.lineHeight}} value={props.text} onChange={handleTextChange} />
-    </div>
-  );
+    return (
+        <div id="text-slider" style={{ position: "absolute", top: props.position }}>
+            <div 
+                id="text-container" 
+                style={{ fontSize: props.fontSize + "px", lineHeight: props.lineHeight }} 
+                contentEditable="true" 
+                value={props.text} 
+                onChange={handleTextChange} >
+                    {props.text}
+            </div>
+        </div>
+    );
 }
 
 export default Slider;
