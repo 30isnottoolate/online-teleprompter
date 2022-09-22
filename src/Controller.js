32d7a27d7694e2a31d75782a62971d6dc9202c48
30isnottoolate <akos.varga.92@gmail.com>
2022-09-22 19:06:42 +0200

    Add handleIsActive to Controller

--- src/Controller.js
+++ src/Controller.js
@@ -13,11 +13,18 @@ const Controller = (props) => {
         props.setTextSpeed(e.target.value);
     }
 
+    const handleIsActive = () => {
+        if (props.isActive) {
+            props.setIsActive(false);
+        } else props.setIsActive(true);
+    }
+
     return (
         <div id="controller">
             <input type="range" id="font-size" min="80" max="150" step="1" value={props.fontSize} onChange={handleFontSize} />
             <input type="range" id="line-height" min="1" max="1.5" step="0.01" value={props.lineHeight} onChange={handleLineHeight} />
             <input type="range" id="text-speed" min="20" max="200" step="1" value={props.textSpeed} onChange={handleTextSpeed} />
+            <button onClick={handleIsActive} >{props.isActive ? "Stop" : "Start"}</button>
         </div>
     );
 }
