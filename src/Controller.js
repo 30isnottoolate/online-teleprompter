ef6553568192c6b6e6d7e99b05723d47134b108a
30isnottoolate <akos.varga.92@gmail.com>
2022-09-24 16:37:58 +0200

    Add labels, implement handleReset

--- src/Controller.js
+++ src/Controller.js
@@ -28,16 +28,25 @@ const Controller = (props) => {
         } else {
             props.setMode("edit");
             props.setIsActive(false);
-            props.setPosition(window.innerHeight * 0.1 + props.fontSize * props.lineHeight);
+            props.setPosition(window.innerHeight * 0.1);
         }
     }
 
+    const handleReset = () => {
+        props.setIsActive(false);
+        props.setPosition(window.innerHeight * 0.1);
+    }
+
     return (
-        <div id="controller">
+        <div id="controller" className={props.isActive ? "transparent" : "visible"}>
+            <label htmlFor="font-size">Font size: </label>
             <input type="range" id="font-size" min="80" max="150" step="1" value={props.fontSize} onChange={handleFontSize} />
+            <label htmlFor="font-size">Line height: </label>
             <input type="range" id="line-height" min="1" max="1.5" step="0.01" value={props.lineHeight} onChange={handleLineHeight} />
+            <label htmlFor="font-size">Text speed: </label>
             <input type="range" id="text-speed" min="20" max="200" step="1" value={props.textSpeed} onChange={handleTextSpeed} />
             <button id="start-stop" onClick={handleIsActive} >{props.isActive ? "Stop" : "Start"}</button>
+            <button id="reset" onClick={handleReset} disabled={props.mode === "edit" ? true : false}>Reset</button>
             <p>Current mode:</p>
             <button id="edit" onClick={handleMode}>{props.mode === "edit" ? "Edit" : "Read"}</button>
         </div>
