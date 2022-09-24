1154dc6f9b61d066d23de7673a0da2f1164cea4d
30isnottoolate <akos.varga.92@gmail.com>
2022-09-24 12:32:56 +0200

    Implement conditional rendering to Slider

--- src/Slider.js
+++ src/Slider.js
@@ -5,18 +5,27 @@ const Slider = (props) => {
         props.setText(e.target.value);
     }
 
-    return (
-        <div id="text-slider">
-            <textarea 
-                id="text-container" 
-                style={{ position: "absolute", top: props.position, fontSize: props.fontSize + "px", lineHeight: props.lineHeight }}
-                value={props.text}
-                onChange={handleTextChange} />
-            <p id ="text-display" style={{ position: "absolute", top: (props.position + 2), left: "2px", fontSize: props.fontSize + "px", lineHeight: props.lineHeight}}>
-                {props.text}
-            </p>
-        </div>
-    );
+    if (props.mode === "edit") {
+        return (
+            <div id="text-slider">
+                <textarea 
+                    id="text-container" 
+                    style={{ position: "absolute", top: props.position, fontSize: props.fontSize + "px", lineHeight: props.lineHeight }}
+                    value={props.text}
+                    onChange={handleTextChange} />
+            </div>
+        );
+    } else {
+        return (
+            <div id="text-slider">
+                <p id ="text-display" style={{ position: "absolute", top: (props.position + 2), left: "2px", fontSize: props.fontSize + "px", lineHeight: props.lineHeight}}>
+                    {props.text}
+                </p>
+            </div>
+        );
+    }
+
+    
 }
 
 export default Slider;
