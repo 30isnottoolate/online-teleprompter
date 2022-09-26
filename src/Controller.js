eb2bda86fddc1366ff3bfda8c11cee89eeda4a67
30isnottoolate <akos.varga.92@gmail.com>
2022-09-26 23:57:25 +0200

    Put elements into div containers

--- src/Controller.js
+++ src/Controller.js
@@ -43,50 +43,65 @@ const Controller = (props) => {
 
     return (
         <div id="controller" className={props.isActive ? "transparent" : "visible"}>
-            <button
-                id="start-stop"
-                onClick={handleIsActive} >
-                {props.isActive ? "Stop" : "Start"}
-            </button>
-            <button
-                id="reset"
-                onClick={handleReset}
-                disabled={props.mode === "edit" ? true : false}>
-                Reset
-            </button>
-            <button id="clear" onClick={handleClear} >Clear</button>
-            <span>Current mode:</span>
-            <button
-                id="mode"
-                onClick={handleMode} >
-                {props.mode === "edit" ? "Edit" : "Read"}
-            </button>
-            <label htmlFor="font-size">Font size: </label>
-            <input
-                id="font-size"
-                type="range" min="80" max="150" step="1"
-                value={props.fontSize}
-                onChange={handleFontSize} />
-            <span>{props.fontSize}</span>
-            <label htmlFor="line-height">Line height: </label>
-            <input
-                id="line-height"
-                type="range" min="1" max="1.5" step="0.01"
-                value={props.lineHeight}
-                onChange={handleLineHeight} />
-            <span>{getLineHeight()}</span>
-            <label htmlFor="text-speed">Text speed: </label>
-            <input
-                id="text-speed"
-                type="range" min="20" max="200" step="1"
-                value={props.textSpeed}
-                onChange={handleTextSpeed} />
-            <span>{props.textSpeed}</span>
-            <button 
-                id="default" 
-                onClick={handleDefault} >
-                Default
-            </button>
+            <div id="logo">
+                <h1>
+                    Online Teleprompter
+                </h1>
+            </div>
+            <div id="main-buttons-group">
+                <button
+                    id="start-stop"
+                    className="main-buttons"
+                    onClick={handleIsActive} >
+                    {props.isActive ? "Stop" : "Start"}
+                </button>
+                <button
+                    id="reset"
+                    className="main-buttons"
+                    onClick={handleReset}
+                    disabled={props.mode === "edit" ? true : false}>
+                    Reset
+                </button>
+                <button id="clear" className="main-buttons" onClick={handleClear} >Clear</button>
+            </div>
+            <div id="mode-group">
+                <span>Current mode: </span>
+                <button
+                    id="mode"
+                    onClick={handleMode} >
+                    {props.mode === "edit" ? "Edit" : "Read"}
+                </button>
+            </div>
+            <div id="settings">
+                <label htmlFor="font-size">Font size: </label>
+                <input
+                    id="font-size"
+                    type="range" min="80" max="150" step="1"
+                    value={props.fontSize}
+                    onChange={handleFontSize} />
+                <span>{props.fontSize}</span>
+                <label htmlFor="line-height">Line height: </label>
+                <input
+                    id="line-height"
+                    type="range" min="1" max="1.5" step="0.01"
+                    value={props.lineHeight}
+                    onChange={handleLineHeight} />
+                <span>{getLineHeight()}</span>
+                <label htmlFor="text-speed">Text speed: </label>
+                <input
+                    id="text-speed"
+                    type="range" min="20" max="200" step="1"
+                    value={props.textSpeed}
+                    onChange={handleTextSpeed} />
+                <span>{props.textSpeed}</span>
+            </div>
+            <div id="default-container">
+                <button
+                    id="default"
+                    onClick={handleDefault} >
+                    Default
+                </button>
+            </div>
         </div>
     );
 }
