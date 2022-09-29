23c354a7e192ebca6242122baf8abc47e4327b44
30isnottoolate <akos.varga.92@gmail.com>
2022-09-29 22:32:52 +0200

    Fix state value initialization with useState
        The theme, text, fontSize, lineHeight and textSpeed initial values
        depend on the existence of local storage values with the same key
        names. If they don't exist, default values will be used. Except for
        fontSize, where viewport width is also taken into consideration.

--- src/Teleprompter.js
+++ src/Teleprompter.js
@@ -13,46 +13,61 @@ const DEFAULT_TEXT_SPEED = "100";
 const Teleprompter = () => {
     const [isActive, setIsActive] = useState(false);
     const [mode, setMode] = useState("edit"); // edit or read
-    const [theme, setTheme] = useState(DEFAULT_THEME);
     const [isMenuEnabled, setIsMenuEnabled] = useState(false);
     const [position, setPosition] = useState(0);
-    const [text, setText] = useState(DEFAULT_TEXT);
-    const [fontSize, setFontSize] = useState(DEFAULT_FONT_SIZE);
-    const [lineHeight, setLineHeight] = useState(DEFAULT_LINE_HEIGHT);
-    const [textSpeed, setTextSpeed] = useState(DEFAULT_TEXT_SPEED);
+
+    const [theme, setTheme] = useState(() => {
+        if (localStorage.getItem("theme") === null) {
+            localStorage.setItem("theme", DEFAULT_THEME);
+            return DEFAULT_THEME;
+        } else {
+            return localStorage.getItem("theme");
+        }
+    });
+    
+    const [text, setText] = useState(() => {
+        if (localStorage.getItem("text") === null) {
+            localStorage.setItem("text", DEFAULT_TEXT);
+            return DEFAULT_TEXT;
+        } else {
+            return localStorage.getItem("text");
+        }
+    });
+
+    const [fontSize, setFontSize] = useState(() => {
+        if (localStorage.getItem("fontSize") === null) {
+            if (window.innerWidth < 701) {
+                localStorage.setItem("fontSize", 40);
+                return 40;
+            } else {
+                localStorage.setItem("fontSize", DEFAULT_FONT_SIZE);
+                return DEFAULT_FONT_SIZE;
+            }
+        } else return localStorage.getItem("fontSize");
+    });
+
+    const [lineHeight, setLineHeight] = useState(() => {
+        if (localStorage.getItem("lineHeight") === null) {
+            localStorage.setItem("lineHeight", DEFAULT_LINE_HEIGHT);
+            return DEFAULT_LINE_HEIGHT;
+        } else {
+            return localStorage.getItem("lineHeight");
+        }
+    });
+
+    const [textSpeed, setTextSpeed] = useState(() => {
+        if (localStorage.getItem("textSpeed") === null) {
+            localStorage.setItem("textSpeed", DEFAULT_TEXT_SPEED);
+            return DEFAULT_TEXT_SPEED;
+        } else {
+            return localStorage.getItem("textSpeed");
+        }
+    });
 
     const textContainerRef = useRef(null);
     const textDisplayRef = useRef(null);
     const textMarkerRef = useRef(null);
-
-    useEffect(() => {
-        if (localStorage.getItem("theme") === null) {
-            localStorage.setItem("theme", DEFAULT_THEME);
-        } else setTheme(localStorage.getItem("theme"));
-
-        if (localStorage.getItem("text") === null) {
-            localStorage.setItem("text", DEFAULT_TEXT);
-        } else setText(localStorage.getItem("text"));
-
-        if (window.innerWidth < 701) {
-            if (localStorage.getItem("fontSize") === null) {
-                localStorage.setItem("fontSize", 40);
-            } else setFontSize(localStorage.getItem("fontSize"));
-        } else {
-            if (localStorage.getItem("fontSize") === null) {
-                localStorage.setItem("fontSize", DEFAULT_FONT_SIZE);
-            } else setFontSize(localStorage.getItem("fontSize"));
-        }
-
-        if (localStorage.getItem("lineHeight") === null) {
-            localStorage.setItem("lineHeight", DEFAULT_LINE_HEIGHT);
-        } else setLineHeight(localStorage.getItem("lineHeight"));
-
-        if (localStorage.getItem("textSpeed") === null) {
-            localStorage.setItem("textSpeed", DEFAULT_TEXT_SPEED);
-        } else setTextSpeed(localStorage.getItem("textSpeed"));
-    }, []);
-
+    
     useEffect(() => {
         if (mode === "edit") textContainerRef.current.focus();
     }, [mode]);
