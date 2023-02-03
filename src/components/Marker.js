f48322e962a11ed930559d510c7f568cd83982ba
30isnottoolate <akos.varga.92@gmail.com>
2023-02-03 19:09:56 +0100

    Change component to use rem units

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Marker = ({ fontSize, lineHeight, color, mode, left, top }) => {
+const Marker = ({ fontSize, lineHeight, color, mode, top }) => {
     return (
         <div>
             <svg
                 className="text-marker"
-                height={fontSize}
+                height={fontSize + "rem"}
                 fill={color}
                 style={{ 
                     display: (mode === "read" ? "initial" : "none"), 
-                    left: left, 
+                    left: `${fontSize * 0.19}rem`, 
                     top: top, 
-                    padding: `${fontSize * (lineHeight - 1) / 2}px 0` 
+                    padding: `${fontSize * (lineHeight - 1) / 2}rem 0` 
                 }}
                 viewBox="0 0 57 150">
                 <path d="M 7.00,45.00 C 7.00,45.00 7.00,111.00 7.00,111.00 7.00,111.00 49.00,78.00 49.00,78.00 49.00,78.00 7.00,45.00 7.00,45.00 Z" />
