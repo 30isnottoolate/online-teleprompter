87a43681399c31ef8c71d9edb78609b879ceda27
30isnottoolate <akos.varga.92@gmail.com>
2023-02-06 14:52:32 +0100

    Create new component

---
+++ src/components/ModeButtons.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ModeButtons =
+    ({ divPresence, changeMode, mode, theme, setTheme }) => {
+
+        const changeTheme = () => setTheme(prevState => prevState === "light" ? "dark" : "light");
+
+        return (
+            <div
+                id="mode-group"
+                style={{ display: divPresence }} >
+                <span>Current mode: </span>
+                <button
+                    id="mode"
+                    className="mode-buttons"
+                    onClick={changeMode} >
+                    {mode === "edit" ? "Edit" : "Read"}
+                </button>
+                <span>Color theme: </span>
+                <button
+                    id="theme"
+                    className="mode-buttons"
+                    onClick={changeTheme} >
+                    {theme === "dark" ? "Dark" : "Light"}
+                </button>
+            </div>
+        );
+    }
+
+export default ModeButtons;
+////
